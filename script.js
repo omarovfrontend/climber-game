de07bef946b14ffd3f4ed002f275e0b92e90f8e6
Shamil Omarov <s_omarov05@mail.ru>
2022-03-24 20:08:08 +0300

    Немного улучшил игру)

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const climber = document.querySelector('.climber-img');
 const winFlag = document.querySelector('.win-img');
 
+const span0 = document.querySelectorAll('span')[0];
+const span1 = document.querySelectorAll('span')[1];
+const span2 = document.querySelectorAll('span')[2];
+const span3 = document.querySelectorAll('span')[3];
+const span4 = document.querySelectorAll('span')[4];
+const span5 = document.querySelectorAll('span')[5];
+const span6 = document.querySelectorAll('span')[6];
+const span7 = document.querySelectorAll('span')[7];
+const span8 = document.querySelectorAll('span')[8];
+const span9 = document.querySelectorAll('span')[9];
+
 const routes = [
   {
     bottom: '20px',
@@ -35,8 +46,30 @@ const routes = [
 const showWinFlag = () => {
   if(count === maxCount) {
     winFlag.style.opacity = 1;
+
+    span0.style.display = 'block';
+    span1.style.display = 'block';
+    span2.style.display = 'block';
+    span3.style.display = 'block';
+    span4.style.display = 'block';
+    span5.style.display = 'block';
+    span6.style.display = 'block';
+    span7.style.display = 'block';
+    span8.style.display = 'block';
+    span9.style.display = 'block';
   } else {
     winFlag.style.opacity = 0;
+
+    span0.style.display = 'none';
+    span1.style.display = 'none';
+    span2.style.display = 'none';
+    span3.style.display = 'none';
+    span4.style.display = 'none';
+    span5.style.display = 'none';
+    span6.style.display = 'none';
+    span7.style.display = 'none';
+    span8.style.display = 'none';
+    span9.style.display = 'none';
   }
 }
 
